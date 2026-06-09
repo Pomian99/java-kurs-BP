fe1589f3564aceb4045cde06f8a1581bcbe5ba9c
Bartosz Pomiankiewicz <01159433@pw.edu.pl>
2026-06-09 18:12:03 +0200

    refactor: fibonacci method uses memory

--- fibonnaci/src/main/java/org/example/Main.java
+++ fibonnaci/src/main/java/org/example/Main.java
@@ -1,14 +1,21 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    private static final ArrayList<Long> fibonacciMemory = new ArrayList<>(List.of((long) 0, (long) 1));
+
     public static void main(String[] args) {
-        System.out.println(fibonacci(120));
+        System.out.println(fibonacci(14));
+        System.out.println(fibonacci(20));
     }
 
-    private static int fibonacci(int n) {
-        if (n <= 1) {
-            return n;
+    private static long fibonacci(int n) {
+        if (fibonacciMemory.size() >= n+1){
+            return fibonacciMemory.get(n);
         }
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        fibonacciMemory.add(fibonacci(n - 1) + fibonacci(n - 2));
+        return fibonacciMemory.get(n);
     }
 }
