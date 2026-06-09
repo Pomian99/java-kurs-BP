8181a1e229a5e47f9db2aa14e54e93347ba45bca
Bartosz Pomiankiewicz <01159433@pw.edu.pl>
2026-06-09 17:50:17 +0200

    feat: fibonacci sequence method

---
+++ fibonnaci/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(120));
+    }
+
+    private static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
