6ce7876701554be5be4f193e1b3f696e0d0253de
Bartosz Pomiankiewicz <01159433@pw.edu.pl>
2026-06-10 18:45:19 +0200

    refactor: BigDecimals created from String and not double

--- ticket/src/main/java/org/example/Main.java
+++ ticket/src/main/java/org/example/Main.java
@@ -4,8 +4,8 @@ import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        Ticket ticket = Ticket.SENIOR;
-        BigDecimal ticketPrice = BigDecimal.valueOf(4.5);
+        Ticket ticket = Ticket.ADULT;
+        BigDecimal ticketPrice = new BigDecimal("4.5");
 
         System.out.printf("%s pays for ticket %.2f.", ticket.getName(), ticket.calculatePrice(ticketPrice));
     }
--- ticket/src/main/java/org/example/Ticket.java
+++ ticket/src/main/java/org/example/Ticket.java
@@ -3,16 +3,16 @@ package org.example;
 import java.math.BigDecimal;
 
 public enum Ticket {
-    CHILD("reduced", 0.5),
-    ADULT("regular", 0.0),
-    SENIOR("senior", 0.8);
+    CHILD("reduced", "0.5"),
+    ADULT("regular", "0.0"),
+    SENIOR("senior", "0.8");
 
     private final String name;
     private final BigDecimal discount;
 
-    Ticket(String name, double discount) {
+    Ticket(String name, String discount) {
         this.name = name;
-        this.discount = BigDecimal.valueOf(discount);
+        this.discount = new BigDecimal(discount);
     }
 
     public String getName() {
